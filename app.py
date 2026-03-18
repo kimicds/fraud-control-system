58e486767d2b421a20678dc075d7f5e785e71fd1
Odunola Akimu Mosebolatan <146633788+kimicds@users.noreply.github.com>
2026-03-18 13:17:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def predict():
 
     session.pop("transaction_data", None)
     return render_template("predict.html", result=result, data=data)
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
